e52c89700092c26c27c8c2b0c60b586fd86bc2da
Jace Riehl <jace.riehl@uleth.ca>
2021-03-03 16:54:51 -0700

    Added all images and JSON objects

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,17 @@ function Image(url, tags, author, link) {
 }
 
 var images = [
-	new Image('imgs/cute-white-dog.jpeg', ["Puppy"], 'PicsbyFran', 'https://pixabay.com/photos/dog-sad-waiting-floor-sad-dog-pet-2785074/'),
+	new Image('imgs/dog1.jpg', ["Puppy", "Cute"], 'PicsbyFran', 'https://pixabay.com/photos/dog-sad-waiting-floor-sad-dog-pet-2785074/'),
+	new Image('imgs/dog2.jpg', ["Adult", "Short-Hair"], 'Vizslafotozas', 'https://pixabay.com/photos/dog-animal-portrait-pet-brown-3277416/'),
+	new Image('imgs/dog3.jpg', ["Adult", "Short-Hair"], 'LUM3N', 'https://pixabay.com/photos/dog-view-sweet-animal-portrait-838281/'),
+	new Image('imgs/dog4.jpg', ["Puppy", "Short-Hair", "Cute"], 'congerdesign', 'https://pixabay.com/photos/dachshund-puppy-young-animal-1519374/'),
+	new Image('imgs/dog5.jpg', ["Puppy", "Short-Hair", "Sleepy"], 'Free-Photos', 'https://pixabay.com/photos/pug-dog-blanket-bed-face-animal-1210025/'),
+	new Image('imgs/dog6.jpg', ["Adult", "Outdoor-Lover", "Medium-Hair"], 'ClaudiaWollesen', 'https://pixabay.com/photos/dog-snow-st-bernard-dog-winter-pet-1168663/'),
+	new Image('imgs/dog7.jpg', ["Adult", "Lab"], 'Chiemsee2016', 'https://pixabay.com/photos/dog-pet-hovawart-black-dog-head-1194087/'),
+	new Image('imgs/dog8.jpg', ["Adult", "Husky"], 'pixel2013', 'https://pixabay.com/photos/husky-dog-dog-breed-animal-3380550/'),
+	new Image('imgs/dog9.jpg', ["Puppy", "Medium-Hair", "Cute"], 'Couleur', 'https://pixabay.com/photos/australian-shepherd-dog-animal-3237735/'),
+	new Image('imgs/dog10.jpg', ["Adult", "Short-Hair", "Lab"], 'Chiemsee2016', 'https://pixabay.com/photos/dog-labrador-pet-dog-head-profile-1194083/'),
+
 ]
 
 var tagList = []
